658ac7bd994d3ec971a80d640354c04943cebfff
andre.dada <kress2m1@gmail.com>
2017-06-28 03:05:44 +0100

    added .gitignore file

--- src/test/java/brownDonaldsonInvestmentTest/LandingPageWrapper.java
+++ src/test/java/brownDonaldsonInvestmentTest/LandingPageWrapper.java
@@ -1,9 +1,6 @@
 package brownDonaldsonInvestmentTest;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import brownDonaldsonInvestment.LandingPage;
 import registeredUserAccount.UserAccountHomePage;
 
